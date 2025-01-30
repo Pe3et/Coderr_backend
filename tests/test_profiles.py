ef4643455bb80711c256a3214144910e24ff2cc7
Pe3et <wolfpeeet@googlemail.com>
2025-01-30 11:44:05 +0100

    finished Registartion testing

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,3 +1,4 @@
+import copy
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
@@ -13,20 +14,6 @@ class TestRegistration(APITestCase):
     }
 
     """
-    Tests if registration with correct data for customers and businesses works as expected.
-    """
-    def test_successful_registration(self):
-        customer_response = self.client.post(self.url, self.correct_customer_data)
-        self.assertEqual(customer_response.status_code, 201)
-
-        correct_business_data = self.correct_customer_data
-        correct_business_data['type'] = 'business'
-        correct_business_data['username'] = 'testbusiness'
-        correct_business_data['email'] = 'testbusiness@example.com'
-        business_response = self.client.post(self.url, correct_business_data)
-        self.assertEqual(business_response.status_code, 201)
-
-    """
     Tests the expected behaviour of trying to register with duplicate data.
     """
     def test_duplicate_data(self):
@@ -36,19 +23,44 @@ class TestRegistration(APITestCase):
         self.assertEqual(response.status_code, 400)
 
     """
-    Tests the expected behaviour of trying to register with incorrect data.
+    Tests if registration with correct data for customers and businesses works as expected.
     """
-    # def test_incorrect_data(self):
-    #     pass
+    def test_successful_registration(self):
+        self.correct_customer_data['username'] = 'testcustomer2'
+        self.correct_customer_data['email'] = 'testcustomer2@example.com'
+        customer_response = self.client.post(self.url, self.correct_customer_data)
+        self.assertEqual(customer_response.status_code, 201)
+
+        correct_business_data = copy.deepcopy(self.correct_customer_data)
+        correct_business_data['type'] = 'business'
+        correct_business_data['username'] = 'testbusiness'
+        correct_business_data['email'] = 'testbusiness@example.com'
+        business_response = self.client.post(self.url, correct_business_data)
+        self.assertEqual(business_response.status_code, 201)
 
     """
-    Tests the correct data structure of the response.
+    Tests the expected behaviour of trying to register with incorrect type.
     """
-    # def test_response_data_structure(self):
-    #     pass
+    def test_incorrect_type(self):
+        incorrect_data = copy.deepcopy(self.correct_customer_data)
+        incorrect_data['username'] = 'incorrect_type_user'
+        incorrect_data['email'] = 'incorrect@type.de'
+        incorrect_data['type'] = 'incorrect_type'
+        response = self.client.post(self.url, incorrect_data)
+        self.assertEqual(response.status_code, 400)
 
     """
     Tests any other request methods than POST.
     """
-    # def test_other_request_methods(self):
-    #     pass
+    def test_other_request_methods(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.put(self.url, self.correct_customer_data)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.patch(self.url, self.correct_customer_data)
+        self.assertEqual(response.status_code, 405)
+
+        response = self.client.delete(self.url, self.correct_customer_data)
+        self.assertEqual(response.status_code, 405)
