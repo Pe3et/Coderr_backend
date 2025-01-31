f775868c7926dbb38aecc6ad3ef858abb51b07e9
Pe3et <wolfpeeet@googlemail.com>
2025-01-31 10:24:53 +0100

    added auth and unauth GET test for single profiles

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -19,11 +19,20 @@ class TestProfiles(APITestCase):
         self.user = serializer.save()
 
     """
-    Tests if the single GET functions properly.
+    Tests if the unauthorized single GET raised 403 Forbidden error.
     """
-    def test_single_profile_get(self):
+    def test_unauth_single_profile_get(self):
         url = reverse('profile-detail', kwargs={'pk':self.user.id})
-        print(url)
+        response = self.client.get(url)
+        print(response.data)
+        self.assertEqual(response.status_code, 403)
+
+    """
+    Tests if the authorized single GET functions properly.
+    """
+    def test_auth_single_profile_get(self):
+        url = reverse('profile-detail', kwargs={'pk':self.user.id})
+        self.client.force_authenticate(user=self.user)
         response = self.client.get(url)
         print(response.data)
         self.assertEqual(response.status_code, 200)
