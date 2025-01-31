53a062c97e18e2a0ea6e6be98b193fa758afa2ba
Pe3et <wolfpeeet@googlemail.com>
2025-01-31 10:20:45 +0100

    added test for single profile GET

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,7 +1,30 @@
 from django.urls import reverse
 from rest_framework.test import APITestCase
 
+from auth_app.api.serializers import RegistrationSerializer
 
-# class TestProfiles(APITestCase):
 
-    # def setUp(self):
+class TestProfiles(APITestCase):
+
+    def setUp(self):
+        test_user_data = {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'password',
+            'repeated_password': 'password',
+            'type': 'customer'
+        }
+        serializer = RegistrationSerializer(data=test_user_data)
+        serializer.is_valid(raise_exception=True)
+        self.user = serializer.save()
+
+    """
+    Tests if the single GET functions properly.
+    """
+    def test_single_profile_get(self):
+        url = reverse('profile-detail', kwargs={'pk':self.user.id})
+        print(url)
+        response = self.client.get(url)
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        
