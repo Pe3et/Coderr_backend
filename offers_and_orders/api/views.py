642b6825af48c9c7d20d477a9be3b63fddec75e6
Pe3et <wolfpeeet@googlemail.com>
2025-02-05 13:15:09 +0100

    added more creator details to offer list view

--- offers_and_orders/api/views.py
+++ offers_and_orders/api/views.py
@@ -24,12 +24,21 @@ class OfferViewSet(viewsets.ModelViewSet):
         return Response(data, status=status.HTTP_200_OK)
     
     """
-    To modify the representation of the offer-details in the list response.
+    To modify the representation of the offer-details in the list response
+    and adds user_details.
     """
     def modify_list_data(self, data):
         for item in data:
             item['details'] = [
-                {"id": detail.id, "url": reverse('offers-detail', kwargs={'pk': detail.id})}
+                {'id': detail.id, 'url': reverse('offers-detail', kwargs={'pk': detail.id})}
                 for detail in Offer.objects.get(id=item['id']).details.all()
             ]
+
+        creator = Offer.objects.get(id=item['id']).creator
+        item['user_details'] = {
+            'first_name': creator.first_name,
+            'last_name': creator.last_name,
+            'username': creator.username
+        }
+    
         return data
