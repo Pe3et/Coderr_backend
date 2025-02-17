b2f2af9429217cefde5b05705520cf0e6de81931
Pe3et <wolfpeeet@googlemail.com>
2025-02-17 09:19:36 +0100

    gave the UserProfile fields empty strings as default values instead of null

--- auth_app/api/models.py
+++ auth_app/api/models.py
@@ -9,9 +9,9 @@ class UserProfile(models.Model):
 
     type = models.CharField(max_length=20, choices=TYPE_CHOICES)
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    location = models.CharField(max_length=20, blank=True, null=True)
+    location = models.CharField(max_length=20, blank=False, default="")
     file = models.FileField(blank=True, null=True, upload_to='profile_pictures/')
-    description = models.TextField(blank=True, null=True)
-    tel = models.CharField(max_length=20, blank=True, null=True)
-    working_hours = models.CharField(max_length=15, blank=True, null=True)
+    description = models.TextField(blank=False, default="")
+    tel = models.CharField(max_length=20, blank=False, default="")
+    working_hours = models.CharField(max_length=30, blank=False, default="")
     uploaded_at = models.DateTimeField(auto_now=True, null=True)
