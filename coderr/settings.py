e2812ab7c57172b2d1a1146902054ad4ebb960a6
Pe3et <wolfpeeet@googlemail.com>
2025-02-18 15:22:37 +0100

    decimals no longer automatically coerce to strings

--- coderr/settings.py
+++ coderr/settings.py
@@ -148,5 +148,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'EXCEPTION_HANDLER': 'coderr.exceptions.custom_exception_handler'
+    'EXCEPTION_HANDLER': 'coderr.exceptions.custom_exception_handler',
+    'COERCE_DECIMAL_TO_STRING': False
 }
